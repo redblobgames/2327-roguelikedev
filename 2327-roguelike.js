1eeb8d27e9a2a258221cc43ba05af664908f7556
Amit Patel <redblobgames@gmail.com>
2023-09-03 15:22:06 -0700

    Refactor: move sprites into their own file

--- 2327-roguelike.js
+++ 2327-roguelike.js
@@ -8,6 +8,7 @@
 
 import {Pos, unlockRoom, unlockableRoomList, generateMap} from "./mapgen.js";
 import {clamp} from "./util.js";
+import sprites from "./sprites.js";
 
 // Drawing area
 /** @type {HTMLCanvasElement} */
@@ -503,44 +504,6 @@ const jobs = {
 function setMessage(str) {
     document.querySelector("#messages").textContent = str;
 }
-
-const sprites = await (async function() {
-    async function S(icon) {
-        // This relies on the way game-icons.net svgs are structured,
-        // as a single <path d="…"/>
-        const url = `./game-icons/${icon}.svg`;
-        const stream = await fetch(url);
-        const svg = await stream.text();
-        return new Path2D(svg.replace(/.* d="/, "").replace(/".*/, ""));
-    }
-
-    return {
-        person:     await S("delapouite/person"),
-        rooster:    await S("delapouite/rooster"),
-        grass:      await S("delapouite/grass"),
-        wheat:      await S("lorc/wheat"),
-        rawfood:    await S("delapouite/grain-bundle"),
-        wall:       await S("delapouite/stone-wall"),
-        cactus:     await S("delapouite/cactus"),
-        door:       await S("delapouite/door"),
-        move:       await S("delapouite/move"),
-        square:     await S("delapouite/square"),
-        sprout:     await S("lorc/sprout"),
-        table:      await S("delapouite/table"),
-        desk:       await S("delapouite/desk"),
-        bed:        await S("delapouite/bed"),
-        digdug:     await S("lorc/dig-dug"),
-        mining:     await S("lorc/mining"),
-        anvil_impact: await S("lorc/anvil-impact"),
-        hand_saw: await S("delapouite/hand-saw"),
-        fishing_pole: await S("delapouite/fishing-pole"),
-        watering_can: await S("delapouite/watering-can"),
-        footprint: await S("lorc/footprint"),
-        stop_sign: await S("delapouite/stop-sign"),
-        check_mark: await S("delapouite/check-mark"),
-    };
-})();
-
 
 const render = {
     /** @type {null | Rect} */
---
+++ sprites.js
@@ -0,0 +1,46 @@
+/*!
+ * From https://www.redblobgames.com/x/2327-roguelike-dev/
+ * Copyright 2023 Red Blob Games <redblobgames@gmail.com>
+ * @license Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+'use strict';
+
+async function S(icon) {
+    // This relies on the way game-icons.net svgs are structured,
+    // as a single <path d="…"/>
+    const url = `./game-icons/${icon}.svg`;
+    const stream = await fetch(url);
+    const svg = await stream.text();
+    return new Path2D(svg.replace(/.* d="/, "").replace(/".*/, ""));
+}
+
+export default {
+    person:       await S("delapouite/person"),
+    rooster:      await S("delapouite/rooster"),
+    grass:        await S("delapouite/grass"),
+    wheat:        await S("lorc/wheat"),
+    rawfood:      await S("delapouite/grain-bundle"),
+    wall:         await S("delapouite/stone-wall"),
+    cactus:       await S("delapouite/cactus"),
+    door:         await S("delapouite/door"),
+    move:         await S("delapouite/move"),
+    square:       await S("delapouite/square"),
+    sprout:       await S("lorc/sprout"),
+    table:        await S("delapouite/table"),
+    desk:         await S("delapouite/desk"),
+    bed:          await S("delapouite/bed"),
+    digdug:       await S("lorc/dig-dug"),
+    mining:       await S("lorc/mining"),
+    anvil_impact: await S("lorc/anvil-impact"),
+    hand_saw:     await S("delapouite/hand-saw"),
+    fishing_pole: await S("delapouite/fishing-pole"),
+    watering_can: await S("delapouite/watering-can"),
+    footprint:    await S("lorc/footprint"),
+    stop_sign:    await S("delapouite/stop-sign"),
+    check_mark:   await S("delapouite/check-mark"),
+    cooking_pot:  await S("delapouite/camp-cooking-pot"),
+    meal:         await S("delapouite/hot-meal"),
+};
+
+
+
